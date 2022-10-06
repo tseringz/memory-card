af169b791fb2dc79f6e2ff2c773822390d547126
tseringz <tserockon77@gmail.com>
2022-10-06 21:24:30 +0530

    Create useEffect hook to show best score pop up modal

--- src/App.js
+++ src/App.js
@@ -1,98 +1,167 @@
-import React, { useEffect, useRef , useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../src/styles/App.css';
 import Card from '../src/components/card';
 import '../src/styles/App.css';
 import naruto from '../src/assets/naruto.png';
 import asuma from '../src/assets/asuma.png';
+import ino from '../src/assets/ino.png';
+import kakazu from '../src/assets/kakazu.png';
+import sasuke from '../src/assets/sasuke.png';
+import chouji from '../src/assets/chouji.png';
+import jiraiya from '../src/assets/jiraiya.png';
+import kiba from '../src/assets/kiba.png';
+import rocklee from '../src/assets/rock-lee.png';
+import shino from '../src/assets/shino.png';
+import gaara from '../src/assets/gaara.png';
+import kakashi from '../src/assets/kakashi.png';
+import minato from '../src/assets/minato.png';
+import sakura from '../src/assets/sakura.png';
+import tobi from '../src/assets/tobi.png';
 import uniqid from 'uniqid';
 
 const initialPictureOrder = [
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
-  {
-      name: 'asuma',
-      src: asuma,
-      id: uniqid(),
-  },
-  {
-      name: 'naruto',
-      src: naruto,
-      id: uniqid(),
-  },
+    {
+        name: 'Naruto',
+        src: naruto,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Asuma',
+        src: asuma,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Ino',
+        src: ino,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Kakazu',
+        src: kakazu,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Sasuke',
+        src: sasuke,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Chouji',
+        src: chouji,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Jiraiya',
+        src: jiraiya,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Kiba',
+        src: kiba,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Rock-lee',
+        src: rocklee,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Shino',
+        src: shino,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Gaara',
+        src: gaara,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Kakashi',
+        src: kakashi,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Minato',
+        src: minato,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Sakura',
+        src: sakura,
+        id: uniqid(),
+        isClick: true,
+    },
+    {
+        name: 'Tobi',
+        src: tobi,
+        id: uniqid(),
+        isClick: true,
+    },
+    
   
-
-]
+  ]
 
 function App() {
-    
 
+    const [ images, setImages ] = useState(initialPictureOrder);
+    const [counter, setCounter] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+    const [showScore, setShowScore] = useState('none');
+
+    function handleClick(index) {
+        const newList = [...images]; 
+        newList.sort(() => Math.random() - 0.5);
+
+        newList[index].isClick = !newList[index].isClick;
+
+        if (!newList[index].isClick && counter < 14) {
+            setCounter(counter + 1);
+            console.log(newList[index].isClick);
+        } else {
+            if(counter > bestScore) {
+                setBestScore(counter);
+            }
+            setCounter(0);
+        }
+        newList.sort(() => Math.random() - 0.5);
+        setImages(newList);
+        
+    }
+
+    useEffect(() => {
+        if (bestScore > 0) {
+            setShowScore('flex');
+        }
+        }, [bestScore]);
 
   return (
     <div className='App'>
-    <Card/>
+        <div style={{
+        display: showScore,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.4)'}}>
+         <h3>Your Best Score: {bestScore}</h3>
+         <button type="button" onClick={() => setShowScore('none')}>Play again</button>
+        </div>
+    <Card images={images} counter={counter} bestScore={bestScore} handleClick={handleClick}/>
   </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,154 +1,13 @@
 
-import React, { useEffect, useState } from 'react';
-import naruto from '../components/assets/naruto.png';
-import asuma from '../components/assets/asuma.png';
-import ino from '../components/assets/ino.png';
-import kakazu from '../components/assets/kakazu.png';
-import sasuke from '../components/assets/sasuke.png';
-import chouji from '../components/assets/chouji.png';
-import jiraiya from '../components/assets/jiraiya.png';
-import kiba from '../components/assets/kiba.png';
-import rocklee from '../components/assets/rock-lee.png';
-import shino from '../components/assets/shino.png';
-import gaara from '../components/assets/gaara.png';
-import kakashi from '../components/assets/kakashi.png';
-import minato from '../components/assets/minato.png';
-import sakura from '../components/assets/sakura.png';
-import tobi from '../components/assets/tobi.png';
-import uniqid from 'uniqid';
+const Card = (props) => {
 
-const nameArrays = [];
-const isClick = true;
-const initialPictureOrder = [
-    {
-        name: 'Naruto',
-        src: naruto,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Asuma',
-        src: asuma,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Ino',
-        src: ino,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Kakazu',
-        src: kakazu,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Sasuke',
-        src: sasuke,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Chouji',
-        src: chouji,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Jiraiya',
-        src: jiraiya,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Kiba',
-        src: kiba,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Rock-lee',
-        src: rocklee,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Shino',
-        src: shino,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Gaara',
-        src: gaara,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Kakashi',
-        src: kakashi,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Minato',
-        src: minato,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Sakura',
-        src: sakura,
-        id: uniqid(),
-        isClick: true,
-    },
-    {
-        name: 'Tobi',
-        src: tobi,
-        id: uniqid(),
-        isClick: true,
-    },
-    
-  
-  ]
-
-const Card = () => {
-    const [ images, setImages ] = useState(initialPictureOrder);
-    const [counter, setCounter] = useState(0);
-    const [bestScore, setBestScore] = useState(0);
-
-
-    function handleClick(index) {
-        const newList = [...images]; 
-        newList[index].isClick = !newList[index].isClick;
-
-        if (!newList[index].isClick) {
-            setCounter(counter + 1);
-            console.log(newList[index].isClick);
-        } else {
-            if(counter > bestScore) {
-                setBestScore(counter);
-            }
-            setCounter(0);
-        }
-
-        newList.sort(() => Math.random() - 0.5);
-        setImages(newList);
-        console.log(newList); 
-    }
-
-
-
-    
     return (
     <div>
-        <span>{counter}</span>
-        <span>BestScore: {bestScore}</span>
+        <span>{props.counter}</span>
+        <span>BestScore: {props.bestScore}</span>
         <div className="card-grid">
-        {images.map((image, i) => (
-    <div key={image.id}  style={{backgroundImage: `url(${image.src})`, backgroundColor: 'blue',}} onClick={() => handleClick(i)}></div>
+        {props.images.map((image, i) => (
+    <div key={image.id}  style={{backgroundImage: `url(${image.src})`, backgroundColor: 'blue',}} onClick={() => props.handleClick(i)}></div>
     ))}
     </div>
     </div>
@@ -156,7 +15,3 @@ const Card = () => {
     }
 
 export default Card;
-const array = [1,2,3,4,5,6,7,8,9,10];
-const array1 = array.sort(() => Math.random() - 0.5);
-
-console.log(array1);
